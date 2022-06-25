cbcc6aaf60cf36d6eca10983415b5feb04ed9ac8
Zakariae Essaiydy <zakaressaiydy@gmail.com>
2022-06-25 10:29:46 +0100

    comment source code

--- src/ma/ensah/test.java
+++ src/ma/ensah/test.java
@@ -1,7 +1,12 @@
 package ma.ensah;
 
+
+//classe test
 public class test {
+    //the main method it's the entry point in the program
     public static void main(String[] args) {
+
+        //print hello to the user
         System.out.println("hello");
     }
 }
